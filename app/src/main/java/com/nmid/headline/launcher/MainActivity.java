a8d09866a518e28e33ce85d895357cb2763141b9
xwysun <xwysun@qq.com>
2017-04-10 16:00:38 +0800

    Day3 Commit1

--- app/src/main/java/com/nmid/headline/launcher/MainActivity.java
+++ app/src/main/java/com/nmid/headline/launcher/MainActivity.java
@@ -1,15 +1,62 @@
 package com.nmid.headline.launcher;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.nmid.headline.R;
+import com.nmid.headline.util.BottomNavigationViewHelper;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
+    @BindView(R.id.bottom_nav)
+    BottomNavigationView bottomNav;
+    @BindView(R.id.contain)
+    FrameLayout contain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        actionBar = getSupportActionBar();
+        BottomNavigationViewHelper.disableShiftMode(bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_nav_news:
+                        Toast.makeText(getApplicationContext(),"news",Toast.LENGTH_SHORT).show();
+                        actionBar.show();
+                        actionBar.setTitle(R.string.bottom_nav_tab1);
+                        break;
+                    case R.id.bottom_nav_newsletter:
+                        Toast.makeText(getApplicationContext(),"newsletter",Toast.LENGTH_SHORT).show();
+                        actionBar.show();
+                        actionBar.setTitle(R.string.bottom_nav_tab2);
+                        break;
+                    case R.id.bottom_nav_teachers:
+                        Toast.makeText(getApplicationContext(),"teacher",Toast.LENGTH_SHORT).show();
+                        actionBar.hide();
+                        actionBar.setShowHideAnimationEnabled(false);
+                        break;
+                    case R.id.bottom_nav_explore:
+                        Toast.makeText(getApplicationContext(),"explore",Toast.LENGTH_SHORT).show();
+                        actionBar.show();
+                        actionBar.setTitle(R.string.bottom_nav_tab4);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
